9085fd0716cdc4bc125626a129f908740b2963d9
kevkipkemoi <kevin.kipngeno@financeplan.biz>
2019-06-29 23:42:11 +0300

    Housekeeping

--- data_scraper.py
+++ data_scraper.py
@@ -4,18 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
 nse_20_url = 'https://www.investing.com/indices/kenya-nse-20-historical-data'
 nse_url = 'https://www.nse.co.ke/'
 start_date = "01/01/2012"
-ab = []
-nd = []
+nse_20_data = []
+list_item = []
 
 def scrape_investing_dot_com(url, start_date):
     # Path to chrome driver if it isn't in Windows PATH
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     sbox = driver.find_element_by_class_name("historicDate")
     sbox.click()
@@ -34,24 +35,25 @@ def scrape_investing_dot_com(url, start_date):
     tables = hist_data.find('table', {'id': 'curr_table'})
     # Remove the newlines 
     daily_data = [[item.text.strip().replace('\n', ',')] for item in tables.find_all('tr')]
-    # Split on the space
+    # Split on the comma
     for index, item in enumerate(daily_data):
         daily_data[index] = daily_data[index][0].split(',')
 
-    for nd in daily_data:
+    # Clean the data
+    for list_item in daily_data:
         # for one list item
-        for index, item in enumerate(nd):
+        for index, item in enumerate(list_item):
             steppr = index + 1
-            if steppr < len(nd) and not index % 2:
+            if steppr < len(list_item) and not index % 2:
                 if index == 0:
-                    ab.append(nd[index].strip() + ' ' + nd[steppr].strip())
+                    nse_20_data.append(list_item[index].strip() + ' ' + list_item[steppr].strip())
                 else:
-                    ab.append(nd[index].strip() + ',' + nd[steppr].strip())
+                    nse_20_data.append(list_item[index].strip() + ',' + list_item[steppr].strip())
             elif steppr == 11:
-                ab.append(nd[index])
+                nse_20_data.append(list_item[index])
     driver.close()
     driver.quit()
-    return ab
+    return nse_20_data
 
 def scrape_nse_dot_com(url):
     page = requests.get(url, verify=False, stream=True)
@@ -59,3 +61,6 @@ def scrape_nse_dot_com(url):
     tickers = soup.find('div', {'class': 'modcontent'}).text.replace(u'\xa0', u'').split('|')
     for ticker in tickers:
     	return ticker
+
+if __name__ == "__main__":
+    scrape = scrape_investing_dot_com(nse_20_url, start_date)
